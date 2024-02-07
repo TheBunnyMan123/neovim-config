e8dfc2f758bfc69fa65a47f092660c4c21097d1f
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-06 22:31:10 -0600

    lsp zero

--- lua/thekillerbunny/lazyload.lua
+++ lua/thekillerbunny/lazyload.lua
@@ -21,6 +21,33 @@ require("lazy").setup({
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     end
   },
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+    config = function()
+      local lsp_zero = require('lsp-zero')
+
+      lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      -- to learn how to use mason.nvim with lsp-zero
+      -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {"lua_ls", "csharp_ls", "java_language_server"},
+        handlers = {
+          lsp_zero.default_setup,
+        },
+      })
+    end
+  },
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
   { "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function()
       require("catppuccin").setup({
